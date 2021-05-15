8f94c282052edb25d95540a9550029a93cb25297
unknown <salimmahat22@gmail.com>
2021-05-15 18:23:49 +0530

    changes

--- client/src/components/constant/Constants.js
+++ client/src/components/constant/Constants.js
@@ -1,5 +1,5 @@
+// var apiBaseUrl = "http://localhost:3000";
 var apiBaseUrl = "https://ipfsnft.herokuapp.com";
-//var apiBaseUrl = "https://ipfsnft.herokuapp.com";
 
 export function getAPiUrl() {
   return apiBaseUrl;

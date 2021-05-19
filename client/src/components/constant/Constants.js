f7544232bcb71879962f59e86f7743beca1f5df7
unknown <salimmahat22@gmail.com>
2021-05-19 20:06:22 +0530

    changes

--- client/src/components/constant/Constants.js
+++ client/src/components/constant/Constants.js
@@ -1,11 +1,11 @@
-var apiBaseUrl = "http://localhost:3000";
-//var apiBaseUrl = "https://ipfsnft.herokuapp.com";
+// var apiBaseUrl = "http://localhost:3000";
+var apiBaseUrl = "https://ipfsnft.herokuapp.com";
 
 export function getAPiUrl() {
   return apiBaseUrl;
 }
 
-var imageBaseUrl = "http://67.205.174.98:8080/ipfs";
+var imageBaseUrl = "http://52.14.165.133:8080/ipfs"; 
 
 export function getimageUrl() {
   return imageBaseUrl;
